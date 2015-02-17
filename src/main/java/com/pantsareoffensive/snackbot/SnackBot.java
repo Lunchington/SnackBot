7e469a9acff1ab1a471d5c83c4914aed546680b0
Lunchington <lordlunchington@gmail.com>
2015-02-16 23:02:28 -0500

    Initial commit

---
+++ src/main/java/com/pantsareoffensive/snackbot/SnackBot.java
@@ -0,0 +1,10 @@
+package com.pantsareoffensive.snackbot;
+
+import org.jibble.pircbot.PircBot;
+
+public class SnackBot extends PircBot {
+   public static void main(String[] args)
+    {
+        System.out.println("Hello");
+    }
+}
