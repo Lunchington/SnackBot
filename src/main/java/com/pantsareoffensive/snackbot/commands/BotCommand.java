d517c56dc45d5bc3901cdff1759256d2e3097b11
Lunchington <lordlunchington@gmail.com>
2015-02-17 16:30:45 -0500

    Basic Mojang server Status

--- src/main/java/com/pantsareoffensive/snackbot/commands/BotCommand.java
+++ src/main/java/com/pantsareoffensive/snackbot/commands/BotCommand.java
@@ -3,9 +3,13 @@ package com.pantsareoffensive.snackbot.commands;
 
 import com.pantsareoffensive.snackbot.Configuration.Config;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 public abstract class BotCommand implements  IBotCommand {
     private String name;
-
+    protected String json;
     public BotCommand(String name) {
         this.name = name;
         loadJson();
@@ -39,6 +43,25 @@ public abstract class BotCommand implements  IBotCommand {
 
     @Override
     public void loadJson() {
+
+    }
+
+    public static String readUrl(String urlString) throws Exception {
+        BufferedReader reader = null;
+        try {
+            URL url = new URL(urlString);
+            reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            StringBuffer buffer = new StringBuffer();
+            int read;
+            char[] chars = new char[1024];
+            while ((read = reader.read(chars)) != -1)
+                buffer.append(chars, 0, read);
+
+            return buffer.toString();
+        } finally {
+            if (reader != null)
+                reader.close();
+        }
 
     }
 }
--- src/main/java/com/pantsareoffensive/snackbot/commands/Mojang.java
+++ src/main/java/com/pantsareoffensive/snackbot/commands/Mojang.java
@@ -2,44 +2,79 @@ package com.pantsareoffensive.snackbot.commands;
 
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.pantsareoffensive.snackbot.SnackBot;
+import org.jibble.pircbot.Colors;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.URL;
-import java.util.List;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Mojang extends BotCommand {
-    private List<String> minecraftStatus; // This is for the inner array
+
+    ArrayList<HashMap<String, String>> jsonObject;
+    HashMap<String, String> shortNames = new HashMap<String, String>()  {};
+
 
     public Mojang() {
+
         super("minecraft");
+        shortNames.put("minecraft.net", "MineCraft.Net");
+        shortNames.put("session.minecraft.net", "Sesssion");
+        shortNames.put("account.mojang.com", "Account");
+        shortNames.put("auth.mojang.com", "Auth");
+        shortNames.put("skins.minecraft.net", "Skins");
+        shortNames.put("authserver.mojang.com", "AuthServer");
+        shortNames.put("sessionserver.mojang.com", "Session");
+        shortNames.put("api.mojang.com", "Api");
+        shortNames.put("textures.minecraft.net", "Textures");
+
     }
 
     @Override
     public void handleMessage(String target, String sender, String login, String hostname, String args) {
-
-
+        loadJson();
+        SnackBot.bot.sendMessage(target, getStatus());
     }
 
     @Override
     public void loadJson() {
         String site  = "http://status.mojang.com/check";
+        Gson gson = new Gson();
 
 
-        Reader reader;
         try {
-            reader = new InputStreamReader(new URL(site).openStream());
-            Gson gson = new GsonBuilder().create();
-            minecraftStatus = gson.fromJson(reader,List.class);
-            System.out.println(minecraftStatus);
+            json = BotCommand.readUrl(site);
+            Type collectionType = new TypeToken<ArrayList<HashMap<String, String>>>(){}.getType();
+            jsonObject = gson.fromJson(json,collectionType);
 
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
 
+
+
+
+    }
+
+    private String getStatus() {
+        String output = "";
+
+        for (HashMap<String, String> g: jsonObject) {
+            for (String key:g.keySet()) {
+                String color =  (g.get(key).toLowerCase().equals("green")) ? Colors.GREEN : Colors.RED ;
+
+                output += color + getShortName(key) + Colors.NORMAL +" ";
+
+            }
+        }
+
+        return output;
+    }
+
+    private String getShortName(String s) {
+        return shortNames.get(s);
     }
 
 
